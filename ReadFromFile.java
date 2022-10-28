a25c037f4bd827474c7c1e23da4ac703d95904b6
Ubuntu <ubuntu@vps-ada68bed.vps.ovh.ca>
2022-10-28 11:49:52 +0800

    first commit

---
+++ ReadFromFile.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ReadFromFile {
+
+	public static void main(String[] args) {
+
+		File file = new File("test.txt");
+		try {
+
+			Scanner sc = new Scanner(file);
+
+			while (sc. hasNextLine())
+				System.out.println(sc.nextLine());
+		} catch (IOException ex) {
+
+			ex. printStackTrace();
+			System.exit(1);
+
+		}
+	}
+}
